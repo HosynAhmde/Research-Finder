1ec3780bf9428bde301d20f8eb9973868f0aaeff
Hossein Ahmadi <hosynahmde@gmail.com>
2023-05-27 22:31:09 +0330

    init elastic

--- src/components/search/search.module.ts
+++ src/components/search/search.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 @Module({
+  imports: [
+    ConfigModule,
+    ElasticsearchModule.register({
+      useFactory: async (configService: ConfigService) => ({
+        import: [ConfigServices],
+        node: configService.get('ELASTICSEARCH_NODE'),
+        auth: {
+          username: configService.get<string>('ELASTICSEARCH_USERNAME'),
+          password: configService.get<string>('ELASTICSEARCH_PASSWORD'),
+        },
+      }),
+    }),
+  ],
   controllers: [SearchController],
-  providers: [SearchService]
+  providers: [SearchService],
 })
 export class SearchModule {}
