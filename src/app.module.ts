041c4204ce71367e1405efe569bce3ce4ade85a8
Hossein Ahmadi <hosynahmde@gmail.com>
2023-05-29 14:44:35 +0330

    Create Docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { MONGO_CONFIG } from '@Common/configs/mongo.config';
 import { ConfigModule } from '@nestjs/config';
 import { ComponentsModule } from '@Components/components.module';
 import { RequestModule } from '@Common/modules';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { SearchModule } from '@Components/search/search.module';
 
 @Module({
   imports: [
